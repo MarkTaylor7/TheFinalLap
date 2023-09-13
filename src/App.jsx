eaf22d4b488df528f313f3eeb6a7303cd809a956
Mark Taylor <mtaylor_001@hotmail.com>
2023-09-12 22:21:17 -0230

    Updates on 09/12/2023 Removed default React components and added a basic dashboard table (Material UI).

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function createData(name, fiveRacesAgo, fourRacesAgo, threeRacesAgo, twoRacesAgo, oneRaceAgo) {
+  return { name, fiveRacesAgo, fourRacesAgo, threeRacesAgo, twoRacesAgo, oneRaceAgo };
 }
 
-export default App
+const rows = [
+  createData('Max Verstappen', 1, 1, 1, 1, 1),
+  createData('Sergio Perez', 6, 3, 2, 4, 2),
+  createData('Fernando Alonso', 262, 16.0, 24, 6.0),
+  createData('Lewis Hamilton', 305, 3.7, 67, 4.3),
+  createData('Carlos Sainz', 356, 16.0, 49, 3.9),
+  createData('Charles Leclerc', 356, 16.0, 49, 3.9),
+];
+
+export default function DenseTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Driver</TableCell>
+            <TableCell align="right">fiveRacesAgo</TableCell>
+            <TableCell align="right">fourRacesAgo</TableCell>
+            <TableCell align="right">threeRacesAgo</TableCell>
+            <TableCell align="right">twoRacesAgo</TableCell>
+            <TableCell align="right">oneRaceAgo</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.fiveRacesAgo}</TableCell>
+              <TableCell align="right">{row.fourRacesAgo}</TableCell>
+              <TableCell align="right">{row.threeRacesAgo}</TableCell>
+              <TableCell align="right">{row.twoRacesAgo}</TableCell>
+              <TableCell align="right">{row.oneRaceAgo}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
