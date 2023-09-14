07de4b41c0cfad57b731111b7e1216bc1967fa38
Mark Taylor <mtaylor_001@hotmail.com>
2023-09-14 15:10:06 -0230

    Changes on 09/14/23 Placed DenseTable on a seperate jsx file called "DenseTable.jsx" Renamed createData function to "formatRow" for better description. Renamed rows object to "racerData" for better description. Added the following to App.jsx:     export default function App() {         return <DenseTable data={racerData} />     } Removed camelCase notation from table headers. Changed align of data cells to "center".

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,20 @@
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import DenseTable from './DenseTable';
 
-function createData(name, fiveRacesAgo, fourRacesAgo, threeRacesAgo, twoRacesAgo, oneRaceAgo) {
+function formatRow(name, fiveRacesAgo, fourRacesAgo, threeRacesAgo, twoRacesAgo, oneRaceAgo) {
   return { name, fiveRacesAgo, fourRacesAgo, threeRacesAgo, twoRacesAgo, oneRaceAgo };
 }
 
-const rows = [
-  createData('Max Verstappen', 1, 1, 1, 1, 2),
-  createData('Sergio Perez', 6, 3, 2, 4, 2),
-  createData('Fernando Alonso', 7, 9, 5, 2, 9),
-  createData('Lewis Hamilton', 3, 4, 4, 6, 6),
-  createData('Carlos Sainz', 10, 8, 'DNF', 5, 3),
-  createData('Charles Leclerc', 9, 7, 3, 'DNF', 4),
+const racerData = [
+  formatRow('Max Verstappen', 1, 1, 1, 1, 1),
+  formatRow('Sergio Perez', 6, 3, 2, 4, 2),
+  formatRow('Fernando Alonso', 7, 9, 5, 2, 9),
+  formatRow('Lewis Hamilton', 3, 4, 4, 6, 6),
+  formatRow('Carlos Sainz', 10, 8, 'DNF', 5, 3),
+  formatRow('Charles Leclerc', 9, 7, 3, 'DNF', 4),
 ];
 
-export default function DenseTable() {
-  return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Driver</TableCell>
-            <TableCell align="right">fiveRacesAgo</TableCell>
-            <TableCell align="right">fourRacesAgo</TableCell>
-            <TableCell align="right">threeRacesAgo</TableCell>
-            <TableCell align="right">twoRacesAgo</TableCell>
-            <TableCell align="right">oneRaceAgo</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.fiveRacesAgo}</TableCell>
-              <TableCell align="right">{row.fourRacesAgo}</TableCell>
-              <TableCell align="right">{row.threeRacesAgo}</TableCell>
-              <TableCell align="right">{row.twoRacesAgo}</TableCell>
-              <TableCell align="right">{row.oneRaceAgo}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+export default function App() {
+  return <DenseTable data={racerData} />
 }
+
---
+++ src/DenseTable.jsx
@@ -0,0 +1,43 @@
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+export default function DenseTable({data}) {
+    return (
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Driver</TableCell>
+              <TableCell align="right">Five Races Ago</TableCell>
+              <TableCell align="right">Four Races Ago</TableCell>
+              <TableCell align="right">Three Races Ago</TableCell>
+              <TableCell align="right">Two Races Ago</TableCell>
+              <TableCell align="right">One Race Ago</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="center">{row.fiveRacesAgo}</TableCell>
+                <TableCell align="center">{row.fourRacesAgo}</TableCell>
+                <TableCell align="center">{row.threeRacesAgo}</TableCell>
+                <TableCell align="center">{row.twoRacesAgo}</TableCell>
+                <TableCell align="center">{row.oneRaceAgo}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  }
