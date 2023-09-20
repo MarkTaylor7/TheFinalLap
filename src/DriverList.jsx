f7103d53e81d9eab3ab92adde096098031c5a98b
Mark Taylor <mtaylor_001@hotmail.com>
2023-09-19 21:40:54 -0230

    Changes on 09/19/23 Successfully converted data from API fetch to array of all drivers' full names and logged in console.

--- src/DriverList.jsx
+++ src/DriverList.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react';
 
+
 const DriverList = () => {
     useEffect(() => {
         const url = "http://ergast.com/api/f1/2023/drivers.json";
@@ -9,11 +10,10 @@ const DriverList = () => {
                 const response = await fetch(url);
                 const json = await response.json();
                 const rawDriverList = json.MRData.DriverTable.Drivers;
-                console.log(rawDriverList);
-                const givenName = json.MRData.DriverTable.Drivers[0].givenName;
-                const familyName = json.MRData.DriverTable.Drivers[0].familyName;
-                const driverName = givenName.concat(' ',familyName);
-                console.log(driverName);
+                const namesDriverList = rawDriverList.map(function(element){
+                    return `${element.givenName} ${element.familyName}`;
+                });
+                console.log(namesDriverList);
                 } catch (error) {
                     console.log("error", error);
                 }
