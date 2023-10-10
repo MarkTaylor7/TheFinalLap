aa7dbe6dd4a3d5df61608496baa35b5c97383036
Mark Taylor <mtaylor_001@hotmail.com>
2023-10-10 13:16:41 -0230

    Changes on 10/10/2023 Refactored the mapNamesToDrivers function to be more efficient, and used the split array method to parse the first names of each driver. First names are set as a key-value pair for each driver, and will be used to filter for race results by matching firstName with givenName in API data.

--- src/App.jsx
+++ src/App.jsx
@@ -28,93 +28,54 @@ export default function App() {
     .then(results => setLastFiveRaceResults(results)); 
   }, []);
 
-
-  const driver1 = [];
-  const driver2 = [];
-  const driver3 = [];
-  const driver4 = [];
-  const driver5 = [];
-  const driver6 = [];
-  const driver7 = [];
-  const driver8 = [];
-  const driver9 = [];
-  const driver10 = [];
-  const driver11 = [];
-  const driver12 = [];
-  const driver13 = [];
-  const driver14 = [];
-  const driver15 = [];
-  const driver16 = [];
-  const driver17 = [];
-  const driver18 = [];
-  const driver19 = [];
-  const driver20 = [];
-  const driver21 = [];
-  const driver22 = [];
-
-  const driverData = [driver1, driver2, driver3, driver4, driver5, driver6,
-     driver7, driver8, driver9, driver10, driver11, driver12,
-      driver13, driver14, driver15, driver16, driver17, driver18, driver19, driver20, driver21, driver22];
-
-  
+  const driverData = [];
 
   function mapNamesToDrivers() {
-    driver1.name = names[0]
-    driver2.name = names[1]
-    driver3.name = names[2]
-    driver4.name = names[3]
-    driver5.name = names[4]
-    driver6.name = names[5]
-    driver7.name = names[6]
-    driver8.name = names[7]
-    driver9.name = names[8]
-    driver10.name = names[9]
-    driver11.name = names[10]
-    driver12.name = names[11]
-    driver13.name = names[12]
-    driver14.name = names[13]
-    driver15.name = names[14]
-    driver16.name = names[15]
-    driver17.name = names[16]
-    driver18.name = names[17]
-    driver19.name = names[18]
-    driver20.name = names[19]
-    driver21.name = names[20]
-    driver22.name = names[21]
+    for (let i = 0; i < 22; i++ ) {
+      const driver = [];
+      const driverNames = [];
+      driverData.push(driver);
+      driverData[i].name = names[i];
+      driverNames.push(driverData[i].name);
+      const nameParts = driverNames[0]?.split(' ');
+      driverData[i].firstName = nameParts[0];
+      driverData[i].lastFiveResults = [];
+    };
   };
 
-  mapNamesToDrivers();
-
+  setTimeout(() => {
+    mapNamesToDrivers();
+  }, 200);
   
-  console.log(driver15);
-    
-
-function mapResultsToDriver() {
-  const fiveRacesAgo = lastFiveRaceResults[4];
-  const fourRacesAgo = lastFiveRaceResults[3];
-  const threeRacesAgo = lastFiveRaceResults[2];
-  const twoRacesAgo = lastFiveRaceResults[1];
-  const oneRaceAgo = lastFiveRaceResults[0];
-  if (oneRaceAgo.Results[0].Driver.givenName === "Max") {
-    driver1.push(oneRaceAgo.Results[0].positionText)};
-};
-
-setTimeout(() => { 
-  mapResultsToDriver();
-}, 200);
-
-console.log(driver1);
-console.log(driverData);
-
-//Have to find way to link DriverData to setDriverResults
-const x = driverData[0];
-
-  function formatRow(name, fiveRacesAgo, fourRacesAgo, threeRacesAgo, twoRacesAgo, x) {
-    return { name, fiveRacesAgo, fourRacesAgo, threeRacesAgo, twoRacesAgo, x };
-  }
+  setTimeout(() => {
+    console.log(driverData);
+  }, 2000);
   
 
-  const racerData = driverData.map(driverData => formatRow(driverData.name, null, null, null, null, x)); 
+  function mapResultsToDriver() {
+    const fiveRacesAgo = lastFiveRaceResults[4];
+    const fourRacesAgo = lastFiveRaceResults[3];
+    const threeRacesAgo = lastFiveRaceResults[2];
+    const twoRacesAgo = lastFiveRaceResults[1];
+    const oneRaceAgo = lastFiveRaceResults[0];
+      if (oneRaceAgo.Results[0].Driver.givenName === driverData[0].firstName) {
+        driverData[0].lastFiveResults.push(oneRaceAgo.Results[0].positionText)};
+  };
+
+  setTimeout(() => { 
+    mapResultsToDriver();
+  }, 500);
+
+
+  //Have to find way to link DriverData to setDriverResults
+
+  
+  function formatRow(name, fiveRacesAgo, fourRacesAgo, threeRacesAgo, twoRacesAgo, oneRaceAgo) {
+    return { name, fiveRacesAgo, fourRacesAgo, threeRacesAgo, twoRacesAgo, oneRaceAgo };
+  };
+  
+  
+  const racerData = driverData.map(driverData => formatRow(driverData.name, null, null, null, null, driverData.lastFiveResults[0])); 
   
   return (
     <>
