ec30f84b28dda0445f043f07e2e59ce0df9674a9
Mark Taylor <mtaylor_001@hotmail.com>
2024-03-12 12:18:20 -0230

    Restored dependency - deploy and test

--- src/App.jsx
+++ src/App.jsx
@@ -664,7 +664,7 @@ export default function App() {
       setLastFiveRaceResults(results);  
       setLastFiveRacesDataFetched(true);      
     }
-  }, [currentSeasonRaceResults]);
+  }, [currentSeasonRaceResults, previousSeasonRaceResults]);
 
   useEffect(() => {
     console.log(lastFiveRaceResults)
